4f56690dc19dd02dc20a5da293d636342c7c9ab7
nelsonGX <hi@nelsongx.com>
2026-03-05 15:58:29 +0800

    Initial Commit

---
+++ coupon-creator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coupon-creator
+//
+//  Created by Nelson Lin on 2026/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coupon-creator/coupon_creatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  coupon_creatorApp.swift
+//  coupon-creator
+//
+//  Created by Nelson Lin on 2026/3/5.
+//
+
+import SwiftUI
+
+@main
+struct coupon_creatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
